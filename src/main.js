8a66c9412d89f0d468530f51005b173f4bfb5565
Leslie B <lesliebuettel@gmail.com>
2023-11-13 14:59:02 +0100

    add close button and animations

---
+++ src/main.js
@@ -0,0 +1,48 @@
+import { gsap } from "gsap"
+
+const DURATION = 0.5;
+
+document.querySelectorAll(".list-item").forEach(item => {
+    item.addEventListener("click", (e) => {
+        const video = document.querySelector(`.video_${e.target.dataset.id}`)
+        const videoElement = video.querySelector(".video-element")
+        const tl = gsap.timeline()
+        tl.to(video, {
+            y: "0%",
+            duration: DURATION
+        })
+            .to(".logo", {
+                scale: 0.65,
+                transformOrigin: "top left",
+                duration: 0.25
+            }, "-=0.25")
+            .from(videoElement, {
+                opacity: 0,
+                duration: DURATION,
+                onComplete: () => {
+                    videoElement.play()
+                }
+            })
+    })
+})
+
+document.querySelectorAll(".video-close").forEach(button => {
+    button.addEventListener("click", (e) => {
+        const wrapper = e.target.parentElement
+        wrapper.style.transform = "translateY(150%)"
+        const video = wrapper.querySelector(".video-element")
+        video.pause();
+        video.currentTime = 0;
+
+        const tl = gsap.timeline()
+        tl.to(wrapper, {
+            y: "150%",
+            duration: DURATION
+        })
+            .to(".logo", {
+                scale: 1,
+                transformOrigin: "top left",
+                duration: 0.25
+            }, "-=0.25")
+    })
+})
